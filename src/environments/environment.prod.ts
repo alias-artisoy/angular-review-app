ee260a1fcefc6beab923d8cd64e0458c242d00be
alias-artisoy <artisoyfernandez@gmail.com>
2021-10-23 14:09:49 +0800

    Update config.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
-};
+
+    export const environment = {
+        production: true,
+        API_URL: "http://localhost:8080/",
+        OAUTH_CLIENT: "angular-review-app-client",
+        OAUTH_SECRET: "angular-review-app-secret"
+    };
